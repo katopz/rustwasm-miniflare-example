58ef22ff618066eeb8614c8b1a49cbd22c1f0f5e
katopz <katopz@gmail.com>
2022-02-19 14:55:55 +0700

    update lib, failed live_reload

--- worker/worker.js
+++ worker/worker.js
@@ -1,10 +1,8 @@
-
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event));
-});
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event))
+})
 
 async function handleRequest(event) {
-  
-  await wasm_bindgen(wasm);
-  return wasm_bindgen.fetch(event.request,{},event);
+  await wasm_bindgen(wasm)
+  return wasm_bindgen.fetch(event.request, {}, event)
 }
